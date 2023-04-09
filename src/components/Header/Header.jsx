c7f9a793e4260d87f91da9e6528561abf52e42f2
IamLikhon <likhonadnan55@gmail.com>
2023-04-09 22:16:28 +0600

    added banner section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,10 +7,10 @@ const Header = () => {
         <div className='flex justify-around mt-10 items-center'>
             <h1 className='text-3xl font-bold '>AL <span className='text-blue-500'>Hubs</span></h1>
             <div>
-            <Link className='mx-8 text-lg font-medium' to='/'>Home</Link>
-            <Link className='mx-8 text-lg font-medium' to='/statistics'>Statistics</Link>
-            <Link className='mx-8 text-lg font-medium' to='/applied'>Applied Jobs</Link>
-            <Link className='mx-8 text-lg font-medium' to='/blog'>Blog</Link>
+            <Link className='mx-6 text-lg font-medium' to='/'>Home</Link>
+            <Link className='mx-6 text-lg font-medium' to='/statistics'>Statistics</Link>
+            <Link className='mx-6 text-lg font-medium' to='/applied'>Applied Jobs</Link>
+            <Link className='mx-6 text-lg font-medium' to='/blog'>Blog</Link>
             </div>
             <button className='nav-btn'>Star Applying</button>
             
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,22 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <p>this is home</p>
+            {/* Banner section start */}
+
+            <div className='md:flex justify-around px-20 mt-20'>
+                <div className="banner-text">
+                    <h1 className='text-7xl font-semibold '>One Step <br /> Closer To Your <br /> <span className='text-blue-500'>Dream Job</span> </h1>
+                    <h3 className='mt-5'>Explore thousands of job opportunities with all the <br /> information you need. Its your future. Come find it. Manage all <br /> your job application from start to finish.</h3>
+                    <button className='nav-btn mt-7'>Get Started</button>
+
+                </div>
+                <div className="banner-img">
+                    <img className='w-full' src="https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80" alt="" />
+
+                </div>
+            </div>
+            {/* Banner section ends */}
+
             
         </div>
     );
